8e2477f64feebc5f1208a560eb3c301885d4461a
Laskellyb <anikwenzekelly@gmail.com>
2022-10-15 15:32:10 +0100

    extended the blob as background but it didn't show up as customised

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       fontFamily: {
         openSans: ["Open Sans", "sans-serif"],
       },
+      animation: {
+        "spin-slow": "spin 60s linear infinite",
+      },
+      backgroundImage: {
+        "red-blob": "url('/src/images/redblob.svg')",
+      },
     },
   },
   plugins: [],
